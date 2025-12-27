c682f525509a643e72c02840c1900bc0d91cb5dc
Md. Meshbaul Haque Tamim <2102028@che.buet.ac.bd>
2025-12-27 12:33:14 +0600

    feat(constants): add VIDEO type to POST_TYPES enum
    Remove unused feed service imports from controller files

--- src/controllers/cr.controllers.js
+++ src/controllers/cr.controllers.js
@@ -2,7 +2,6 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { ApiError } from "../utils/ApiError.js";
 import { POST_TARGET_MODELS, POST_TYPES } from "../constants/index.js";
-import { getCrFeedService } from "../services/academic.service.js";
 import {
   createPostService,
   toggleLikePostService,
